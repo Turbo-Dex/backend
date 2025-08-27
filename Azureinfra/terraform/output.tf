adbdaa659ab32b78d504381ad7edf16a8ab5c690
Dani <dani.fariadossantos@heig-vd.ch>
2025-08-27 18:54:17 +0200

    chore : fixed erros

--- Azureinfra/terraform/output.tf
+++ Azureinfra/terraform/output.tf
@@ -6,14 +6,6 @@ output "storage_account_name" {
   value = azurerm_storage_account.sa.name
 }
 
-output "db_host" {
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
 
 output "db_host" {
   value = azurerm_postgresql_flexible_server.pg.fqdn
@@ -31,7 +23,4 @@ output "analyse_lb_ip" {
   value = kubernetes_service.analyse_lb.status[0].load_balancer[0].ingress[0].ip
 }
 
-output "loadbalancer_ip" {
-  value = kubernetes_service.app_lb.status[0].load_balancer[0].ingress[0].ip
-}
 
